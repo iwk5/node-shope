c18c974fbd6c004093cbb678fbfccf5291d99ce2
waqas khan <waqaskhanwb@gmail.com>
2019-10-12 15:58:37 +0500

    mongo uri changed

--- app.js
+++ app.js
@@ -19,10 +19,11 @@ const shopController = require('./controllers/shop');
 const isAuth = require('./middleware/is-auth');
 const User = require('./models/user');
 
-const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${
+/*const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${
   process.env.MONGO_PASSWORD
   }@cluster0-c5rtn.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`;
-//mongodb + srv://wikibabu:<password>@cluster0-c5rtn.mongodb.net/test?retryWrites=true&w=majority
+*/
+const MONGODB_URI = `mongodb + srv://wikibabu:<unlock1%M>@cluster0-c5rtn.mongodb.net/shopt?retryWrites=true&w=majority`;
 const app = express();
 const store = new MongoDBStore({
   uri: MONGODB_URI,
