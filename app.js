ddee5046d922b48e49814616474793b61a62ed5d
waqas khan <waqaskhanwb@gmail.com>
2019-10-12 16:32:43 +0500

    some fixes

--- app.js
+++ app.js
@@ -19,11 +19,9 @@ const shopController = require('./controllers/shop');
 const isAuth = require('./middleware/is-auth');
 const User = require('./models/user');
 
-/*const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${
   process.env.MONGO_PASSWORD
   }@cluster0-c5rtn.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`;
-*/
-const MONGODB_URI = `mongodb + srv://wikibabu:<unlock1md>@cluster0-c5rtn.mongodb.net/shopt?retryWrites=true&w=majority`;
 const app = express();
 const store = new MongoDBStore({
   uri: MONGODB_URI,
